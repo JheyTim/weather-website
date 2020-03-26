46b789ac77d3d78a9f9dc0875619f2a51fc3a6b1
JheyTim <jheytim.rnr@gmail.com>
2020-03-27 00:08:55 +0800

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,8 @@ const search = document.querySelector('input');
 const messageOne = document.getElementById('message-1');
 const messageTwo = document.getElementById('message-2');
 
-// messageOne.textContent = "java"
-
 const fetchLocation = (location) => {
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
         .then((res) => res.json())
         .then(({ error, location, forecast }) => {
             if (error) {
